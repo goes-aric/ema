87bcd26ba10c83aa08b2422f76e819257025b6f7
Goes Arik <53406260+goes-aric@users.noreply.github.com>
2022-10-31 15:53:18 +0800

    Update AkunResource.php
    Updated Akun Module
    1. Updated Akun Resource Due to New Kode Akun Format

--- app/Http/Resources/Akun/AkunResource.php
+++ app/Http/Resources/Akun/AkunResource.php
@@ -13,9 +13,38 @@ class AkunResource extends JsonResource
             'nama_akun'     => $this->nama_akun,
             'akun_utama'    => $this->akun_utama,
             'induk'         => $this->akunInduk ?? null,
+            'tipe_akun_id'  => $this->akunArray($this->tipe_akun),
             'tipe_akun'     => $this->tipe_akun,
             'kode_user'     => $this->createdUser->kode_user,
             'nama_user'     => $this->createdUser->nama ?? null
         ];
+    }
+
+    public function akunArray($name){
+        $akun = [
+            [
+                'id'    => '1-',
+                'name'  => 'AKTIVA'
+            ],
+            [
+                'id'    => '2-',
+                'name'  => 'KEWAJIBAN'
+            ],
+            [
+                'id'    => '3-',
+                'name'  => 'EKUITAS'
+            ],
+            [
+                'id'    => '4-',
+                'name'  => 'PENDAPATAN'
+            ],
+            [
+                'id'    => '5-',
+                'name'  => 'BEBAN'
+            ]
+        ];
+
+        $key = array_search($name, array_column($akun, 'name'));
+        return $akun[$key]['id'];
     }
 }
