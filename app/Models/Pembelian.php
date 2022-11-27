7ef71259c5128a277d58e30b2ee7b21b90fc12b5
emawidhiani <115211465+emawidhiani@users.noreply.github.com>
2022-11-27 10:40:01 +0800

    Update Pembelian.php

--- app/Models/Pembelian.php
+++ app/Models/Pembelian.php
@@ -4,11 +4,12 @@ namespace App\Models;
 
 use App\Models\BaseModel;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Database\Eloquent\SoftDeletes;
+// use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Pembelian extends BaseModel
 {
-    use Notifiable, SoftDeletes;
+    // use Notifiable, SoftDeletes;
+    use Notifiable;
 
     protected $searchable = [
         'columns' => [
